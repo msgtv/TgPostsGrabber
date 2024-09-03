7de67b59e6b086c3f5d681be267899f8c52a8d20
msgtv <linar_97@list.ru>
2024-09-03 12:16:47 +0500

    GOOD

--- src/telethon/dialogs.py
+++ src/telethon/dialogs.py
@@ -3,7 +3,7 @@ from src.settings import DIALOGS_FILENAME
 
 def load_dialogs():
     with open(DIALOGS_FILENAME, encoding='utf-8') as f:
-        data = f.read()
+        data = f.read().strip()
 
     dialogs = []
     for row in data.split('\n'):
